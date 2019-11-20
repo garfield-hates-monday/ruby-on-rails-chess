9cf6528a38fc12928083111643a7da2d13aa4b7b
Gregory Eglen <eglen.g@gmail.com>
2019-11-20 05:37:37 +0000

    Removed first_name, last_name, picture from Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_20_035842) do
+ActiveRecord::Schema.define(version: 2019_11_20_053604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,9 +50,6 @@ ActiveRecord::Schema.define(version: 2019_11_20_035842) do
     t.datetime "updated_at", null: false
     t.string "provider"
     t.string "uid"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "picture"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
