07c5e4bfce883ec291e4d33634877a213cccf870
Gregory Eglen <eglen.g@gmail.com>
2019-12-02 06:18:15 +0000

    removed name validation for game and applicable Rspec test

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -18,9 +18,7 @@ RSpec.describe GamesController, type: :controller do
     end
 
     it "should properly deal with validation errors" do
-      post :create, params: { game: { name: '' } }
-      expect(response).to have_http_status(:unprocessable_entity)
-      expect(Game.count).to eq Game.count
+
     end
   end
 end
