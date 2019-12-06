765b96cac835db7f22fbd5415cebc2fcf0bb9bc0
Gregory Eglen <eglen.g@gmail.com>
2019-12-06 08:09:21 +0000

    got links to all divs

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -7,9 +7,19 @@ class PiecesController < ApplicationController
   end
 
   def update
-    @piece = Piece.find_by_id(params[:id])
+    @piece = Piece.find(params[:id])
     @game = @piece.game
+    @piece.update_attributes(piece_params)
     @piece.move_to!(:x_position, :y_position)
     redirect_to game_url(@game)
   end
+
+  private
+
+  def piece_params
+    params.require(:piece).permit(:x_position, :y_position, :type)
+  end
 end
+
+
+
