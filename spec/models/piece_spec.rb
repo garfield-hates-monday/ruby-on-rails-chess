3fe517a476c5a838d1413d567bedf896f52ee262
Gregory Eglen <eglen.g@gmail.com>
2019-12-06 06:22:18 +0000

    Fixed Rspec Tests for King

--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -2,18 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Piece, type: :model do
   describe "King" do
+    let(:game) { Game.create!(name: "Test Game" ) }
+    let(:king) { King.create!( game: game, :x_position => 4, :y_position => 1, :color => "white" ) }
     it "King.create should create a new King piece" do
-      king = King.create
       expect(king.valid?).to eq true
     end
 
     it "should return true for a valid move" do
-     king = King.create( :x_position => 4, :y_position => 1, :color => "white" )
      expect(king.valid_move?(5, 2)).to eq true
     end
 
     it "should return false for an invalid move" do
-      king = King.create( :x_position => 4, :y_position => 1, :color => "white" )
       expect(king.valid_move?(9,9)).to eq false
     end
   end
