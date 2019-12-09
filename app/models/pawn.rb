8bc1c977f33ea5dd23569218a0d9d5d735f177eb
Trey Guy <tguy728@gmail.com>
2019-12-09 17:51:51 +0000

    added pawn movement

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,3 +1,17 @@
 class Pawn < Piece
+  attr_accessor :on_initial_square, :color
+  
+
+  def valid_move?(x,y)
+    if @on_initial_square && x_difference(x) == 0 && y_difference(y) == 2
+      return true
+    elsif !@on_initial_square && x_difference(x) == 0 && y_difference(y) == 1
+      return true
+    else 
+      return false
+    end
+
+
+  end
 
 end
