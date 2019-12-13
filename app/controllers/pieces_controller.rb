7bfabda893a681d31c4d02914cd868023a32dc9d
Gregory Eglen <eglen.g@gmail.com>
2019-12-13 07:23:26 +0000

    IT WORKS! Pieces can move and capture other pieces!

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -9,15 +9,15 @@ class PiecesController < ApplicationController
   def update
     @piece = Piece.find(params[:id])
     @game = @piece.game
-    @piece.update_attributes(piece_params)
-    @piece.move_to!(:x_position, :y_position)
+    #@piece.update_attributes(piece_params)
+    @piece.move_to!(params[:x_position], params[:y_position])
     redirect_to game_url(@game)
   end
 
   private
 
   def piece_params
-    params.require(:piece).permit(:x_position, :y_position, :type)
+    params.permit(:x_position, :y_position, :type)
   end
 end
 
