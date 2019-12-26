622bc1889b0ecb32569a5c45a0e84a41d242863c
Conner Krall <conner.krall@gmail.com>
2019-12-26 16:57:37 +0000

    get most of the syntax cleaned up. still need to figure out diagonal capture

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,30 +1,28 @@
 class Pawn < Piece
 
   def valid_move?(x,y)
-    x_distance = ( x - x_position ).abs
-    y_distance = (y - y_position ).abs
     if en_passant?(x, y)
       return true 
     #black diagonal capture
-    elsif (x_distance == y_distance) && !white? && is_obsturcted?(x, y, id, color)
-      y == y_coord + 1
+    elsif y == (self.y_position - 1) && x_difference(x) == y_difference(y) && !white? && occupied?(x,y)
+      return true
     #white diagonal capture
-    elsif (x_distance == y_distance) && white? && is_obsturcted?(x, y, id, color)
-      new_y_coord == y_coord - 1
+    elsif y == (self.y_position + 1) && x_difference(x) == y_difference(y) && white? && occupied?(x,y)
+      return true
     #black starting position (move 2)
-    elsif y_coord == 2 && black? && !is_obsturcted?(x, y, id, color)
-      x_distance == 0 && (y == 3 || y == 4)
+    elsif y_position == 2 && !white? && !is_obstructed?(x, y) && y == (self.y_position + 2) && x_difference(x) == 0
+      return true
     #white starting position (move 2)
-    elsif y_coord == 7 && white? && !is_obsturcted?(x, y, id, color)
-      x_distance == 0 && (y == 6 || y == 5)
+    elsif y_position == 7 && white? && !is_obstructed?(x, y) && y == (self.y_position - 2) && x_difference(x) == 0
+      return true
     #black post initial move
-    elsif !white? && !is_obsturcted?(x, y, id, color)
-      (x_distance == 0) && (x == (y + 1))
+    elsif !white? && !is_obstructed?(x, y) && x_difference(x) == 0 && y == (self.y_position + 1)
+      return true
     #white post initial move
-    elsif white? && !is_obsturcted?(x, y, id, color)
-      (x_distance == 0) && (new_y_coord == (y_coord - 1))
+    elsif white? && !is_obstructed?(x, y) && x_difference(x) == 0 && y == (self.y_position - 1)
+      return true
     else
-      false
+      return false
     end
   end
 
@@ -33,7 +31,7 @@ class Pawn < Piece
 
 
   def en_passant?(new_x_position, new_y_position)
-    return false unless ((new_y_position == y_position + 1 && !white?) || (new_y_position == y_position - 1 && white?)) && ((new_x_coord == x_coord + 1) || (new_x_coord == x_coord - 1)) && ((new_y_coord == 3 && white?) || (new_y_coord == 6 && !white?))
+    return false unless ((new_y_position == (y_position + 1) && !white?) || (new_y_position == (y_position - 1) && white?)) && new_x_position == (x_position + 1) || new_x_position == (x_position - 1) && new_y_position == 3 && white? || new_y_position == 6 && !white?
     other_piece = game.pieces.where(y_position: y_position, x_position: new_x_position, type: "Pawn").first
     return false if other_piece.nil? || other_piece.move_number != 1
     return true
@@ -41,6 +39,6 @@ class Pawn < Piece
 
   def pawn_promotion?
     pawn = game.pieces.where(:type =>"Pawn").where(:user_id => game.turn_user_id)[0]
-    (y_coord == 8 && !white?) || (y_coord == 1 && white?) #black pawn white baseline or white pawn black baseline
+    (y_position == 8 && !white?) || (y_position == 1 && white?) #black pawn white baseline or white pawn black baseline
   end
 end
