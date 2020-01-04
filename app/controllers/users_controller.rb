82e9cb782dc719ff7f92e8b9aa4c795f36fbd96a
Conner Krall <conner.krall@gmail.com>
2020-01-04 05:55:06 +0000

    fix user controller for active games

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @active_games = current_user.games.where(state: nil)
-    @completed_games = current_user.games.where(state: 'completed')
+    @completed_games = current_user.games.where(state: 'end')
   end
 
 
