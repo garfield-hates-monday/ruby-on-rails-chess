281d81dcdd30c59674737a16fa6cae869d66824a
Conner Krall <conner.krall@gmail.com>
2020-01-04 19:12:01 +0000

    track wins/losses/draws

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @active_games = current_user.games.where(state: nil)
     @completed_games = current_user.games.where(state: 'end')
+    @user_wins = current_user.games.where(state: 'end').where(winner_user_id: current_user.id).count
+    @user_losses = current_user.games.where(state: 'end').where(loser_user_id: current_user.id).count
+    @user_draws = current_user.games.where(state: 'stalemate').count
   end
-
-
-
 end
