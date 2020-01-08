f6c9435473d4632820af84086706d0eef3d5198f
Conner Krall <conner.krall@gmail.com>
2020-01-08 22:10:34 +0000

    fix user dashboard for unmatched games

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @active_games = current_user.games.where(state: nil)
+    @active_games = current_user.games.where(state: nil).where.not(black_user_id: nil)
     @completed_games = current_user.games.where(state: 'end')
     @user_wins = current_user.games.where(state: 'end').where(winner_user_id: current_user.id).count
     @user_losses = current_user.games.where(state: 'end').where(loser_user_id: current_user.id).count
